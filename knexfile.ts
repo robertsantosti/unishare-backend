4cc634ac4cd22866a077ee52dc06bbb529c2c52a
robertsantosspm365 <robertsantosti@outlook.com>
2020-10-09 21:47:48 -0300

    configurations

--- knexfile.ts
+++ knexfile.ts
@@ -20,32 +20,38 @@ module.exports = {
   staging: {
     client: "postgresql",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: "d136cbjgoaf989",
+      user: "ngsapnchvtveqk",
+      password: "9687577f77b4aab681ef4cc9ebfe6ffa037326470c8f3f50cfed1eb7db8b91ea"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
+      directory: path.resolve(__dirname, "src", "database", "migrations")
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds")
     }
   },
 
   production: {
     client: "postgresql",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: "d136cbjgoaf989",
+      user: "ngsapnchvtveqk",
+      password: "9687577f77b4aab681ef4cc9ebfe6ffa037326470c8f3f50cfed1eb7db8b91ea"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
+      directory: path.resolve(__dirname, "src", "database", "migrations")
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds")
     }
   }
 };
